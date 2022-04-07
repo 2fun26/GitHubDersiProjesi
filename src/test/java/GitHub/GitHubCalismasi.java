084d12d73da14318863c36b5526dd8eeee5c864e
OmerTUFAN <98853013+2fun26@users.noreply.github.com>
2022-04-07 02:38:18 +0100

    Update GitHubCalismasi.java

--- src/test/java/GitHub/GitHubCalismasi.java
+++ src/test/java/GitHub/GitHubCalismasi.java
@@ -5,6 +5,7 @@ public class GitHubCalismasi {
     public static void main(String[] args) {
 
         System.out.println("GitHub Dersi Basladi!");
-
+        
+        System.out.println("GitHub Dersinde Basarilar");
     }
 }
