fe911a1fca0583d6d31960646a84516a3f2cb002
OMER <omertufan26@yandex.com>
2022-04-07 21:20:47 +0100

    ucuncu gonderim

---
+++ src/test/java/GitHub/GitHubEgzersiz.java
@@ -0,0 +1,12 @@
+package GitHub;
+
+public class GitHubEgzersiz {
+
+    public static void main(String[] args) {
+
+        System.out.println("Yeni Calisma");
+
+        System.out.println("Birseyler yazdim.");
+        System.out.println("Yeni eklemeler");
+    }
+}
